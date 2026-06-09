a93e606e6c62553588eaa7b1b55abf1ebb4145ad
Sergey Wince <w1nce@yandex.ru>
2026-06-09 12:13:38 +0300

    Update main.tf

--- 02/wrk/main.tf
+++ 02/wrk/main.tf
@@ -8,26 +8,39 @@ resource "yandex_vpc_subnet" "develop" {
   v4_cidr_blocks = var.default_cidr
 }
 
+resource "yandex_vpc_subnet" "develop_db" {
+  name           = "${var.vpc_name}-db"
+  zone           = var.vm_db_zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = ["10.0.2.0/24"]
+}
 
 data "yandex_compute_image" "ubuntu" {
-  family = var.vm_web_image_family
+  family = var.vm_image_family
 }
-resource "yandex_compute_instance" "platform" {
+
+# 1 VM
+
+resource "yandex_compute_instance" "platform_web" {
   name        = var.vm_web_name
   platform_id = var.vm_web_platform_id
+
   resources {
     cores         = var.vm_web_cores
     memory        = var.vm_web_memory
     core_fraction = var.vm_web_core_fraction
   }
+
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu.image_id
     }
   }
+
   scheduling_policy {
     preemptible = var.vm_web_preemptible
   }
+
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
     nat       = var.vm_web_nat
@@ -37,5 +50,38 @@ resource "yandex_compute_instance" "platform" {
     serial-port-enable = var.vm_web_serial_port_enable
     ssh-keys           = "${var.vm_web_ssh_user}:${var.vms_ssh_root_key}"
   }
+}
 
+# 2 VM
+
+resource "yandex_compute_instance" "platform_db" {
+  name        = var.vm_db_name
+  platform_id = var.vm_db_platform_id
+  zone        = var.vm_db_zone
+
+  resources {
+    cores         = var.vm_db_cores
+    memory        = var.vm_db_memory
+    core_fraction = var.vm_db_core_fraction
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+
+  scheduling_policy {
+    preemptible = var.vm_db_preemptible
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop_db.id
+    nat       = var.vm_db_nat
+  }
+
+  metadata = {
+    serial-port-enable = var.vm_db_serial_port_enable
+    ssh-keys           = "${var.vm_db_ssh_user}:${var.vms_ssh_root_key}"
+  }
 }
