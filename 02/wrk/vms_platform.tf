35e3452366a26fb299687c4bd8cba9e707438969
Sergey Wince <w1nce@yandex.ru>
2026-06-09 12:09:17 +0300

    Update vms_platform.tf

--- 02/wrk/vms_platform.tf
+++ 02/wrk/vms_platform.tf
@@ -1,108 +1,103 @@
-# 1 VM
-
-variable "vm_web_image_family" {
-  type        = string
-  default     = "ubuntu-2004-lts"
+variable "vm_image_family" {
+  type    = string
+  default = "ubuntu-2004-lts"
 }
 
+# 1 VM
+
 variable "vm_web_name" {
-  type        = string
-  default     = "netology-develop-platform-web"
+  type    = string
+  default = "netology-develop-platform-web"
 }
 
 variable "vm_web_platform_id" {
-  type        = string
-  default     = "standard-v3"
+  type    = string
+  default = "standard-v3"
 }
 
 variable "vm_web_cores" {
-  type        = number
-  default     = 2
+  type    = number
+  default = 2
 }
 
 variable "vm_web_memory" {
-  type        = number
-  default     = 1
+  type    = number
+  default = 1
 }
 
 variable "vm_web_core_fraction" {
-  type        = number
-  default     = 20
+  type    = number
+  default = 20
 }
 
 variable "vm_web_preemptible" {
-  type        = bool
-  default     = true
+  type    = bool
+  default = true
 }
 
 variable "vm_web_nat" {
-  type        = bool
-  default     = true
+  type    = bool
+  default = true
 }
 
 variable "vm_web_serial_port_enable" {
-  type        = number
-  default     = 1
+  type    = number
+  default = 1
 }
 
 variable "vm_web_ssh_user" {
-  type        = string
-  default     = "ubuntu"
+  type    = string
+  default = "ubuntu"
 }
 
 # 2 VM
 
-variable "vm_db_image_family" {
-  type        = string
-  default     = "ubuntu-2004-lts"
-}
-
 variable "vm_db_name" {
-  type        = string
-  default     = "netology-develop-platform-db"
+  type    = string
+  default = "netology-develop-platform-db"
 }
 
 variable "vm_db_platform_id" {
-  type        = string
-  default     = "standard-v3"
+  type    = string
+  default = "standard-v3"
 }
 
 variable "vm_db_cores" {
-  type        = number
-  default     = 2
+  type    = number
+  default = 2
 }
 
 variable "vm_db_memory" {
-  type        = number
-  default     = 2
+  type    = number
+  default = 2
 }
 
 variable "vm_db_core_fraction" {
-  type        = number
-  default     = 20
+  type    = number
+  default = 20
 }
 
 variable "vm_db_preemptible" {
-  type        = bool
-  default     = true
+  type    = bool
+  default = true
 }
 
 variable "vm_db_nat" {
-  type        = bool
-  default     = false
+  type    = bool
+  default = false
 }
 
 variable "vm_db_serial_port_enable" {
-  type        = number
-  default     = 1
+  type    = number
+  default = 1
 }
 
 variable "vm_db_ssh_user" {
-  type        = string
-  default     = "ubuntu"
+  type    = string
+  default = "ubuntu"
 }
 
 variable "vm_db_zone" {
-  type        = string
-  default     = "ru-central1-b"
+  type    = string
+  default = "ru-central1-b"
 }
