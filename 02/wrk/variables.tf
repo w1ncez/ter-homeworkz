996d045e3aa96b036418ef14615cf2fa7e82123b
Sergey Wince <w1nce@yandex.ru>
2026-06-09 10:40:28 +0300

    Update variables.tf

--- 02/wrk/variables.tf
+++ 02/wrk/variables.tf
@@ -3,11 +3,13 @@
 
 variable "cloud_id" {
   type        = string
+  default     = "b1gqaphc31rehpt*****"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
 }
 
 variable "folder_id" {
   type        = string
+  default     = "b1go1u1uj0hvmdf*****"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
 
@@ -29,59 +31,59 @@ variable "vpc_name" {
 }
 
 variable "vm_web_image_family" {
-  type        = string
+  type    = string
   default = "ubuntu-2004-lts"
 }
 
-variable "vm_web_na" {
-  type        = string
-  default     = "netology-develop-platform-web"
+variable "vm_web_name" {
+  type    = string
+  default = "netology-develop-platform-web"
 }
 
 variable "vm_web_platform_id" {
-  type        = string
-  default     = "standard-v3"
+  type    = string
+  default = "standard-v3"
 }
 
 variable "vm_web_cores" {
-  type        = number
-  default     = 2
+  type    = number
+  default = 2
 }
 
 variable "vm_web_memory" {
-  type        = number
-  default     = 1
+  type    = number
+  default = 1
 }
 
 variable "vm_web_core_fraction" {
-  type        = number
-  default     = 20
+  type    = number
+  default = 20
 }
 
 variable "vm_web_preemptible" {
-  type        = bool
-  default     = true
+  type    = bool
+  default = true
 }
 
 variable "vm_web_nat" {
-  type        = bool
-  default     = true
+  type    = bool
+  default = true
 }
 
 variable "vm_web_serial_port_enable" {
-  type        = number
-  default     = 1
+  type    = number
+  default = 1
 }
 
 variable "vm_web_ssh_user" {
-  type        = string
-  default     = "ubuntu"
+  type    = string
+  default = "ubuntu"
 }
 
 ###ssh vars
 
+
 variable "vms_ssh_root_key" {
-  type        = string
-  default     = "<your_ssh_ed25519_key>"
-  description = "ssh-keygen -t ed25519"
+  type    = string
+  default = "ssh-ed25519*******"
 }
