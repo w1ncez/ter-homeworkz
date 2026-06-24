8e872e16c84400629276a8cf79349e92ac262064
Sergey Wince <w1nce@yandex.ru>
2026-06-24 17:07:58 +0300

    Create variables.tf

---
+++ 03/wrk/variables.tf
@@ -0,0 +1,30 @@
+###cloud vars
+
+variable "cloud_id" {
+  type        = string
+  default     = "b1gqaphc31reh****"
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  default     = "b1go1u1uj0hvmd****"
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+variable "default_cidr" {
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "develop"
+  description = "VPC network & subnet name"
+}
